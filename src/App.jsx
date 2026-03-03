7753eab2d65ec62a16cb22129408baccac1b6f1c
Quantum <aburorhambagadaniel@gmail.com>
2026-03-03 17:51:12 +0100

    starting code

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect, Fragment } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [isOn, setIsOn] = useState(false);
+  const [bulb, setBulb] = useState('src/assets/download (2).jpg');
+
+
+  function togglebutton(e) {
+    setIsOn(e.target.checked);
+  }
+  useEffect(() => {
+    if (isOn) {
+      setBulb('src/assets/download (1) .png')
+    }
+
+    else {
+      setBulb('src/assets/download (2).png')
+    }
+
+
+  }, [isOn]);
+
+
+
+
+  return (
+    <Fragment>
+      <div className="form-check form-switch text-white">
+        <input className="form-check-input" type="checkbox" id="mySwitch" checked={isOn} onChange={togglebutton} />
+        <label className="form-check-label" for="mySwitch">Click to switch the bulb</label>
+      </div>
+      <img src={bulb} alt="bulb image" height="150" width="100" className='mt-4' />
+
+    </Fragment>
+  )
+}
+
+export default App
